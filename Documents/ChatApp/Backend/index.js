eb0b397b72867b0973731583c401bd41558332b7
Sai Nikhil Reddy <sainikhil3009@gmail.com>
2024-09-08 20:43:10 -0400

    Basic socket connection

---
+++ Documents/ChatApp/Backend/index.js
@@ -0,0 +1,30 @@
+/** @format */
+
+import express from 'express';
+import dotenv from 'dotenv';
+import http from 'http';
+import { Server } from 'socket.io';
+
+dotenv.config();
+const PORT = process.env.PORT || 3030;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+	cors: {
+		allowedHeaders: ['*'],
+		origin: '*',
+	},
+});
+io.on('connection', (socket) => {
+	console.log('client connected');
+	socket.on('chat msg', (msg) => {
+		console.log('received msg' + msg);
+	});
+});
+app.get('/', (req, res) => {
+	res.send('Hello, World!');
+});
+
+server.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
---
+++ Documents/ChatApp/frontend/there-client/src/app/page.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import Image from 'next/image';
+import Chat from './chat';
+
+export default function Home() {
+	return (
+		<>
+			<Chat />
+		</>
+	);
+}
